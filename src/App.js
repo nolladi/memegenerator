922b6ef25a82320e9432cb56cd6f5c46cb39224c
nolladi <96886835+nolladi@users.noreply.github.com>
2022-01-23 13:59:12 +0100

    Memegenerator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MemeHeader from "./components/MemeHeader";
+import MemeBody from "./components/MemeBody";
+import html2canvas from "html2canvas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            topText:"",
+            botText:"",
+            randomImage:"http://i.imgflip.com/1bij.jpg",
+            allMemeImgs:[],
+            blackOrWhite:"black",
+        }
+    this.handleChange=this.handleChange.bind(this)
+    this.handleSumbit=this.handleSumbit.bind(this)
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response=>response.json())
+            .then(response=>{
+                const {memes}=response.data
+                this.setState({allMemeImgs:memes})
+            })
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({[name]: value})
+
+
+
+
+    }
+
+    handleSumbit(event){
+    event.preventDefault()
+        html2canvas(document.querySelector("#capture"), {
+            allowTaint: true,
+            useCORS: true
+        })
+            .then(canvas => {
+            //document.body.appendChild(canvas)
+
+                let dt = canvas.toDataURL('image/jpeg');
+
+                const w = window.open('about:blank', 'image from canvas');
+                w.document.write("<img src='"+dt+"' alt='from canvas'/>");
+                console.log('Saved!');
+
+        });
+
+    }
+
+
+    render() {
+
+        return (
+
+        <div>
+            <MemeHeader/>
+            <MemeBody
+                handleSubmit={this.handleSumbit}
+                handleFunction={this.handleChange}
+                blackOrWhite={this.state.blackOrWhite}
+                topText={this.state.topText}
+                botText={this.state.botText}
+                randomImage={this.state.randomImage}
+                allMemeImgs={this.state.allMemeImgs}
+
+            />
+        </div>
+        )
+    }
 }
 
 export default App;
